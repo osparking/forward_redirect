f1970d601e1e4c32b0dbc63ae196d372fe521ddc
osparking <jbpark03@gmail.com>
2024-06-09 15:54:25 +0900

    지불 금액 전달 모델 속성

--- src/main/java/space/bum/smvc/PaymentController.java
+++ src/main/java/space/bum/smvc/PaymentController.java
@@ -2,6 +2,7 @@ package space.bum.smvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,10 @@ public class PaymentController {
   @GetMapping("/pay_form")
   public String showPayForm() {
     return "pay_form";
+  }
+  
+  @ModelAttribute(name = "payment")
+  public Payment payment() {
+    return new Payment();
   }
 }
