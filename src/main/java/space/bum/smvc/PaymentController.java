efeab33cebc2e863d24af67f4615cf4dac4cc7f3
osparking <jbpark03@gmail.com>
2024-06-09 19:06:20 +0900

    납입 결과 처리 - redirect

--- src/main/java/space/bum/smvc/PaymentController.java
+++ src/main/java/space/bum/smvc/PaymentController.java
@@ -12,24 +12,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping()
 @Slf4j
 public class PaymentController {
-  
+
   @PostMapping("/pay_result")
   public String payResult2(@ModelAttribute Payment payment) {
     log.info("post 결과 금액: {}", payment.toString());
     return "pay_result";
   }
-  
+
   @PostMapping("/pay_form")
   public String processPayment(@ModelAttribute Payment payment) {
     log.info("공과금 지불 금액: {}", payment.toString());
-    return "forward:pay_result";
+    return "redirect:pay_result";
   }
-  
+
+  @GetMapping("/pay_result")
+  public String payResult1(@ModelAttribute Payment payment) {
+    log.info("get 결과 금액: {}", payment.toString());
+    return "pay_result";
+  }
+
   @GetMapping("/pay_form")
   public String showPayForm() {
     return "pay_form";
   }
-  
+
   @ModelAttribute(name = "payment")
   public Payment payment() {
     return new Payment();
