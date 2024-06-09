88e00bf6a302845a6e6bc4fa6e9920c18828c2a2
osparking <jbpark03@gmail.com>
2024-06-09 12:37:45 +0900

    forward 와redirect 차이 실습

---
+++ src/main/java/space/bum/smvc/ForwardRedirectApplication.java
@@ -0,0 +1,13 @@
+package space.bum.smvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForwardRedirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForwardRedirectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/smvc/ForwardRedirectApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.smvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForwardRedirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
